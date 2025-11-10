b05314ba84e16e09b920f159aae94c4a7b1e5e3d
amul087 <darshuimage@gmail.com>
2025-11-10 21:30:18 +0530

    Add word count functionality using PySpark

---
+++ p5.py
@@ -0,0 +1,14 @@
+from google.colab import files
+uploaded = files.upload()
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("word_count").getOrCreate()
+
+text_file = spark.read.text("txtt.txt")
+
+words = text_file.rdd.flatMap(lambda line: line.value.split(" "))
+
+word_count = words.count()
+
+print("Number of words in text file:", word_count)
